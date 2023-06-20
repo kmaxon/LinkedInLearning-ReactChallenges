9b2386b2be5f96d56d829e475a5bd909d4bd1ec1
keith <maxonkeith@gmail.com>
2023-06-20 15:36:48 -0500

    window event - event listeners

--- src/06-add-window-event/WindowEvent.js
+++ src/06-add-window-event/WindowEvent.js
@@ -1,4 +1,13 @@
+import { useEffect } from 'react';
+
 export default function WindowEvent () {
+  useEffect(() => {
+    const doubleClick = () => alert('mouse pressed')
+
+    window.addEventListener('dblclick', doubleClick)
+
+    return () => window.removeEventListener('dblclick', doubleClick)
+  }, [])
   return (
     <h2>Window event active</h2>
   )
