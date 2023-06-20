37116a208846eee57c5c38b76b492edc0a71bbab
keith <maxonkeith@gmail.com>
2023-06-20 13:50:46 -0500

    final dog pic generator

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css'
-import DadJokes from './04-dog-pics/DogPics'
+import DogPics from './04-dog-pics/DogPics'
 
 function App () {
   return (
     <div className='App'>
-      <DadJokes />
+      <DogPics />
     </div>
   )
 }
