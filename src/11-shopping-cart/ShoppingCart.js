45151bfc3c798bf217bad0899481eb35ef90a1e3
keith <maxonkeith@gmail.com>
2023-06-22 14:59:47 -0500

    shopping cart - could probablybe cleaner

--- src/11-shopping-cart/ShoppingCart.js
+++ src/11-shopping-cart/ShoppingCart.js
@@ -2,18 +2,46 @@ import { useState } from 'react'
 
 const items = [{
   name: 'apple',
-  price: 0.39
+  price: 0.39,
+  quantity: 0
 }, {
   name: 'banana',
-  price: 0.79
+  price: 0.79,
+  quantity: 0
 }, {
   name: 'cherry tomatoes',
-  price: 3.99
+  price: 3.99,
+  quantity: 0
 }]
 
 function ShoppingCart () {
-  const cart = [{ name: 'apple', quantity: 3, price: 0.39 }]
+  const [cart, setCart] = useState([]);
+  const [total, setTotal] = useState((0).toFixed(2));
 
+  function addToCart(item) {
+    if (item.quantity === 0) {
+      item.quantity += 1;
+      setCart([...cart, item]);
+    } else if (item.quantity >= 1) {
+      item.quantity += 1;
+      const newCart = items.filter(item => item.quantity >= 1)
+      setCart(newCart);
+      
+      }
+    console.log(cart)
+    let newTotal = 0;
+    for (let i = 0; i < items.length; i++) {
+      newTotal += (items[i].quantity * items[i].price)
+    }
+    setTotal(newTotal.toFixed(2))
+  }
+
+  function clearButton() {
+    setCart([]);
+    setTotal(0);
+    items.map(item => item.quantity = 0)
+  }
+  
   return (
     <div>
       <h1>Shopping Cart</h1>
@@ -24,7 +52,7 @@ function ShoppingCart () {
             <div key={item.name}>
               <h3>{item.name}</h3>
               <p>${item.price}</p>
-              <button>Add to Cart</button>
+              <button onClick={() => addToCart(item)} >Add to Cart</button>
             </div>)
           )}
         </div>
@@ -38,13 +66,14 @@ function ShoppingCart () {
                 {item.quantity}
                 <button>+</button>
               </p>
-              <p>Subtotal: ${item.quantity * item.price}</p>
+              <p>Subtotal: ${(item.quantity * item.price).toFixed(2)}</p>
             </div>
           ))}
         </div>
       </div>
       <div className='total'>
-        <h2>Total: $0.00</h2>
+        <h2>Total: ${total}</h2>
+        <button onClick={() => clearButton()}>Clear</button>
       </div>
     </div>
   )
